7ceb0a6e67d6817eb7cb2e538f707c35e649821c
kerthcet <kerthcet@gmail.com>
2024-07-04 01:04:23 +0800

    Add Remove Element
    Signed-off-by: kerthcet <kerthcet@gmail.com>

--- README.md
+++ README.md
@@ -4,10 +4,14 @@
 
 ## Algorithms
 
-- [Merge Sorted Array](./src/algos/merge_sorted_array.rs)
+- Top Interview 150
+  - [Merge Sorted Array](./src/algos/merge_sorted_array.rs)
+  - [Remove Element](./src/algos/remove_element.rs)
 
 ## Data Structures
 
-- [FIFO Queue](./src/data-structures/fifo.rs)
-- [Singly Linked List](./src/data-structures/singly_linked_list.rs)
-- [Doubly Linked List](./src/data-structures/doubly_linked_list.rs)
+- Queue
+  - [FIFO Queue](./src/data-structures/fifo.rs)
+- Linked List
+  - [Doubly Linked List](./src/data-structures/doubly_linked_list.rs)
+  - [Singly Linked List](./src/data-structures/singly_linked_list.rs)
---
+++ src/algos/remove_element.rs
@@ -0,0 +1,45 @@
+// Description: https://leetcode.cn/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150
+
+struct Solution;
+
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        let mut m:i8 = 0;
+        let mut n:i8 = nums.len() as i8 - 1;
+
+        while m <= n {
+            if nums[m as usize] == val {
+                if nums[n as usize] == val {
+                    n -= 1;
+                    continue
+                }else{
+                    nums[m as usize] = nums[n as usize];
+                    n -= 1;
+                }
+            }
+            m += 1;
+        }
+        return m as i32
+    }
+}
+
+fn main() {
+    let mut nums1: Vec<i32> = vec![0,1,2,2,3,0,4,2];
+    let value1: i32 = Solution::remove_element(&mut nums1, 2);
+    assert_eq!(5, value1, "value1 not right");
+    assert_eq!(vec![0,1,4,0,3], nums1[0..5], "nums1 not right");
+
+    let mut nums2: Vec<i32> = vec![];
+    let value2: i32 = Solution::remove_element(&mut nums2, 0);
+    assert_eq!(0, value2, "value2 not right");
+    let vec2: Vec<i32> = vec![];
+    assert_eq!(vec2, nums2[0..0], "nums2 not right");
+
+    let mut nums3: Vec<i32> = vec![1];
+    let value3: i32 = Solution::remove_element(&mut nums3, 1);
+    assert_eq!(0, value3, "value3 not right");
+    let vec3: Vec<i32> = vec![];
+    assert_eq!(vec3, nums3[0..0], "nums3 not right");
+
+    println!("Succeed!")
+}
